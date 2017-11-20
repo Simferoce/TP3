5844d94c202da8025b35d19d9407fe3f1ed68be6
Simon <le6monstre@hotmail.com>
2017-11-20 14:52:48 -0500

    Monter les documents

---
+++ TP3Solution/TP3SolutionTest/unittest1.cpp
@@ -0,0 +1,14 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+//Il faut que ce dernier lien soit aussi dans 
+//[right-click sur projet]\éditeur de liens\propriétés\entrées\dépendances additionnelles
+//sinon il y aura une erreur de linkage
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ProjetSFMLTest
+{		
+	TEST_CLASS(UnitTest1)
+	{
+	};
+}
